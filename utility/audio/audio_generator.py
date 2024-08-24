7ba6058701bef8932b4db1f977ed58753ddaff6a
barisx <barissenyerli@gmail.com>
2024-08-24 17:18:35 +0300

    fix audio generator

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,7 +1,7 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural", pitch="-50Hz", volume="+20dB")
+    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural", pitch="-50Hz", volume="+20%")
     await communicate.save(outputFilename)
 
 
