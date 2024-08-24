1a21813587bbc018421001e9574aae4543963e0d
barisx <barissenyerli@gmail.com>
2024-08-24 16:49:21 +0300

    audio bold

--- utility/audio/audio_generator.py
+++ utility/audio/audio_generator.py
@@ -1,7 +1,7 @@
 import edge_tts
 
 async def generate_audio(text,outputFilename):
-    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural")
+    communicate = edge_tts.Communicate(text,"en-AU-WilliamNeural", pitch="-50Hz", speed="-50%", volume="+20dB")
     await communicate.save(outputFilename)
 
 
